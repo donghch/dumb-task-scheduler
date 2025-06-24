5074da2a26ffe8fb395893f1d63de2e65d213e2d
donghch <dieshenke@gmail.com>
2025-06-24 10:39:03 -0700

    Cortex-M3 startup code

--- src/hardware/startup/startup-cortex-m3.c
+++ src/hardware/startup/startup-cortex-m3.c
@@ -6,10 +6,11 @@ extern int main(void);
 
 extern int systick_handler(void);
 
-extern uint32_t _stacktop; // Defined in linker script
+extern uint32_t __stacktop; // Defined in linker script
+void startup(void);
 
 const uint32_t vectors[] __attribute__((section(".isr_vector"))) = {
-    (uint32_t)(0x20001000),      // Initial stack pointer
+    (uint32_t)(&__stacktop),      // Initial stack pointer
     (uint32_t)(main),  // Reset handler
     (uint32_t)0x3,    // NMI handler
     (uint32_t)0x4, // Hard Fault handler
@@ -27,3 +28,28 @@ const uint32_t vectors[] __attribute__((section(".isr_vector"))) = {
     (uint32_t)(systick_handler), // SysTick handler
     // Add other exception handlers here
 };
+
+extern uint32_t __data_start;
+extern uint32_t __data_end;
+extern uint32_t __data_load;
+extern uint32_t __bss_start;
+extern uint32_t __bss_end;
+
+void startup(void) {
+
+    // copy data section
+    uint32_t *p = &__data_start;
+    uint32_t *q = &__data_load;
+    while (p < &__data_end) {
+        *p++ = *q++;
+    }
+
+    // zero bss section
+    p = &__bss_start;
+    while (p < &__bss_end) {
+        *p++ = 0;
+    }
+
+    // Call main function
+    main();
+}
