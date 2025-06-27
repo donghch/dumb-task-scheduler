b72834b29f4c3b5d817da629d303b1b1beb47731
donghch <dieshenke@gmail.com>
2025-06-26 20:43:01 -0700

    fix context switch handling so it get data from psp

--- src/src/hardware/cortex-m3/startup.s
+++ src/src/hardware/cortex-m3/startup.s
@@ -15,15 +15,16 @@ systick_handler:
 
     # save all registers to current context  
     LDR r0, =current_context
+    MRS r2, PSP
 
     # save r0-r3
-    LDR r1, [sp, #0]
+    LDR r1, [r2, #0]
     STR r1, [r0]
-    LDR r1, [sp, #4]
+    LDR r1, [r2, #4]
     STR r1, [r0, #4]
-    LDR r1, [sp, #8]
+    LDR r1, [r2, #8]
     STR r1, [r0, #8]
-    LDR r1, [sp, #12]
+    LDR r1, [r2, #12]
     STR r1, [r0, #12]
     # save r4-r11
     STR r4, [r0, #16]
@@ -34,15 +35,17 @@ systick_handler:
     STR r9, [r0, #36]
     STR r10, [r0, #40]
     STR r11, [r0, #44]
-    # save r12, lr, pc, xPSR
-    LDR r1, [sp, #16]
+    # save r12, lr, sp, pc, xPSR
+    LDR r1, [r2, #16]
     STR r1, [r0, #48]
-    LDR r1, [sp, #20]
+    LDR r1, [r2, #20]
     STR r1, [r0, #52]
-    LDR r1, [sp, #24]
+    MRS r1, PSP
     STR r1, [r0, #56]
-    LDR r1, [sp, #28]
+    LDR r1, [r2, #24]
     STR r1, [r0, #60]
+    LDR r1, [r2, #28]
+    STR r1, [r0, #64]
 
     # call the C handler
     PUSH {lr}
@@ -50,15 +53,19 @@ systick_handler:
     POP {lr}
 
     # restore the context
+    # load back sp first
+    LDR r2, [r0, #56]
+    MSR PSP, r2
+    ISB
     # restore r0-r3
     LDR r1, [r0]
-    STR r1, [sp, #0]
+    STR r1, [r2, #0]
     LDR r1, [r0, #4]
-    STR r1, [sp, #4]
+    STR r1, [r2, #4]
     LDR r1, [r0, #8]
-    STR r1, [sp, #8]
+    STR r1, [r2, #8]
     LDR r1, [r0, #12]
-    STR r1, [sp, #12]
+    STR r1, [r2, #12]
     # restore r4-r11
     LDR r4, [r0, #16]
     LDR r5, [r0, #20]
@@ -68,15 +75,15 @@ systick_handler:
     LDR r9, [r0, #36]
     LDR r10, [r0, #40]
     LDR r11, [r0, #44]
-    # restore r12, lr, pc, xPSR
+    # restore r12, lr, sp, pc, xPSR
     LDR r1, [r0, #48]
-    STR r1, [sp, #16]
+    STR r1, [r2, #16]
     LDR r1, [r0, #52]
-    STR r1, [sp, #20]
-    LDR r1, [r0, #56]
-    STR r1, [sp, #24]
+    STR r1, [r2, #20]
     LDR r1, [r0, #60]
-    STR r1, [sp, #28]
+    STR r1, [r2, #24]
+    LDR r1, [r0, #64]
+    STR r1, [r2, #28]
 
     # restore CONTROL register
     BX lr
