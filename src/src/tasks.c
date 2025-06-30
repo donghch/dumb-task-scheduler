426dd7d52875bb196289d26f5eeeef36c4528f5b
donghch <dieshenke@gmail.com>
2025-06-30 12:35:54 -0700

    dummy code for lock testing

--- src/src/tasks.c
+++ src/src/tasks.c
@@ -1,16 +1,32 @@
 
 #include "types/spinlock.h"
+#include "types/lock.h"
 
 spinlock_t lock;
+task_t *wait_array[10];
+lock_t lc = {
+    .guard = {0},
+    .flag = 0,
+    .wait_queue = {
+        .tasks = wait_array,
+        .capacity = 10,
+        .size = 0,
+        .head = 0,
+        .tail = 0
+    }
+};
+
+static int counter = 0;
 
 void idle_task(void** args) {
     int a = 0;
 
-    spinlock_acquire(&lock);
+    lock_acquire(&lc);
     while (a < 5) {
         a++;
+        counter++;
     }
-    spinlock_release(&lock);
+    lock_release(&lc);
 
     while (1) {
         ;
@@ -19,11 +35,12 @@ void idle_task(void** args) {
 
 void dumb_task(void **args) {
     int b = 0;
-    spinlock_acquire(&lock);
+    lock_acquire(&lc);
     while (b < 5) {
         b++;
+        counter++;
     }
-    spinlock_release(&lock);
+    lock_release(&lc);
     while (1) {
         ;
     }
@@ -31,12 +48,13 @@ void dumb_task(void **args) {
 
 void random_task(void **args) {
     int c = 0;
-    spinlock_acquire(&lock);
+    lock_acquire(&lc);
     while (c < 5) {
         c++;
+        counter++;
     }
-    spinlock_release(&lock);
+    lock_release(&lc);
     while (1) {
-        asm ("SVC #158");
+        ;
     }
 }
